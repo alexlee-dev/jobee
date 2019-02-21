07b7b51698d5d55c2bc4dd9f6e41b7d0be7404f4
Alex Lee <alex@alex-lee.site>
2019-02-21 06:37:46 -0800

    ✏️ Modification to Firebase

--- api/lib/firebase/config.js
+++ api/lib/firebase/config.js
@@ -4,12 +4,25 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv.default.config();
+
+var apiKey = process.env.FIREBASE_API_KEY;
+var authDomain = process.env.FIREBASE_AUTH_DOMAIN;
+var databaseURL = process.env.FIREBASE_DATABASE_URL;
+var projectId = process.env.FIREBASE_PROJECT_ID;
+var storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+var messagingSenderId = proccess.env.FIREBASE_MESSAGING_SENDER_ID;
 var _default = {
-  apiKey: 'AIzaSyDl4JQtZFD0SMRiGUqHiLZMUGXDjWrU_aM',
-  authDomain: 'jobee-a97f0.firebaseapp.com',
-  databaseURL: 'https://jobee-a97f0.firebaseio.com',
-  projectId: 'jobee-a97f0',
-  storageBucket: 'jobee-a97f0.appspot.com',
-  messagingSenderId: '394234248282'
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId
 };
 exports.default = _default;
