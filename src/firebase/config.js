b8a5b7b51f98c612292524505febb61c73c44e15
Alex Lee <alex@alex-lee.site>
2019-02-23 08:09:01 -0800

    ✏️ Fixed firebase import

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,9 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-const apiKey = process.env.FIREBASE_API_KEY
-const authDomain = process.env.FIREBASE_AUTH_DOMAIN
-const databaseURL = process.env.FIREBASE_DATABASE_URL
-const projectId = process.env.FIREBASE_PROJECT_ID
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET
-const messagingSenderId = proccess.env.FIREBASE_MESSAGING_SENDER_ID
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY
+const authDomain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+const databaseURL = process.env.REACT_APP_FIREBASE_DATABASE_URL
+const projectId = process.env.REACT_APP_FIREBASE_PROJECT_ID
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 
 export default {
   apiKey,
--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -5,33 +5,33 @@ const db = firebase.firestore()
 
 export const testDB = () => console.log({ db })
 
-/**
- * Stores data in a document.
- * @param {String} documentId Unique string to identify the document.
- * @param {Object} data Object of data to store in the document.
- */
-export const storeNewDocument = (documentId, data) =>
-  db
-    .collection('jobs')
-    .doc(documentId)
-    .set(data)
+// /**
+//  * Stores data in a document.
+//  * @param {String} documentId Unique string to identify the document.
+//  * @param {Object} data Object of data to store in the document.
+//  */
+// export const storeNewDocument = (documentId, data) =>
+//   db
+//     .collection('jobs')
+//     .doc(documentId)
+//     .set(data)
 
-export const checkIfDocumentExists = documentId => {
-  return new Promise((resolve, reject) => {
-    // Collection
-    const collectionRef = db.collection('jobs')
-    // Document
-    const docRef = collectionRef.doc(documentId)
+// export const checkIfDocumentExists = documentId => {
+//   return new Promise((resolve, reject) => {
+//     // Collection
+//     const collectionRef = db.collection('jobs')
+//     // Document
+//     const docRef = collectionRef.doc(documentId)
 
-    docRef
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          resolve(true)
-        } else {
-          resolve(false)
-        }
-      })
-      .catch(error => reject(error))
-  })
-}
+//     docRef
+//       .get()
+//       .then(doc => {
+//         if (doc.exists) {
+//           resolve(true)
+//         } else {
+//           resolve(false)
+//         }
+//       })
+//       .catch(error => reject(error))
+//   })
+// }
