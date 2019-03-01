ec0f4fe79c1f4ebcd508b5e1ff0aec482d8cfb27
Alex Lee <alex@alex-lee.site>
2019-02-28 22:28:51 -0800

    ✏️ Undo the CI change

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app'
 import config from './config'
 
-if (process.env.CI !== 'true') {
-  firebase.initializeApp(config)
-}
+console.log({ config })
+
+firebase.initializeApp(config)
