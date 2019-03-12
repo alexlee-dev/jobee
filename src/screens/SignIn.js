96d40455770b732d528742b886cc645057f00d55
Alex Lee <alex@alex-lee.site>
2019-03-11 21:09:21 -0700

    ✨ 25 - InfoTag
    Adds an info tag to show version # and Netlify build status.

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Box } from 'grommet'
+import { Box, Text, Image } from 'grommet'
 import SignInButtons from '../components/SignInButtons'
 import Logo from '../components/Logo'
+import { version } from '../../package.json'
 
 export const SignIn = () => {
   return (
@@ -9,6 +10,13 @@ export const SignIn = () => {
       <Box animation={{ type: 'fadeIn', duration: 3000 }} gap="xlarge">
         <Logo />
         <SignInButtons />
+      </Box>
+      <Box direction="row" gap="xsmall">
+        <Text size="small" weight="bold">
+          Version:
+        </Text>
+        <Text size="small">{version}</Text>
+        <Image src="https://api.netlify.com/api/v1/badges/81706173-76e4-4215-bf6f-517365c762cf/deploy-status" />
       </Box>
     </Box>
   )
